4d843a11cf757ef6d2ff6534a1c6b757b91eedda
Daniel Chcouri <333222@gmail.com>
2014-09-19 07:34:36 +0300

    Bump version to v1.0.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tap:i18n',
   summary: 'A comprehensive internationalization solution for Meteor',
-  version: '1.0.3',
+  version: '1.0.4',
   git: 'https://github.com/TAPevents/tap-i18n'
 });
 
