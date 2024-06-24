ec455490fa62a052f601c73f08c417654a825403
Daniel C <daniel@meteorspark.com>
2024-06-24 16:05:42 +0700

    Bump version to v1.12.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tap:i18n',
   summary: 'A comprehensive internationalization solution for Meteor',
-  version: '1.11.2',
+  version: '1.12.0',
   git: 'https://github.com/TAPevents/tap-i18n'
 });
 
